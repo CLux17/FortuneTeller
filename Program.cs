b97d889a267ae8a1eab33be3b5426e05f5e2b0d2
CLux <cplux7@gmail.com>
2019-05-15 10:04:25 -0400

    added somemore

--- Program.cs
+++ Program.cs
@@ -9,20 +9,27 @@ namespace FortuneTeller
             Console.WriteLine("Greetings!! and welcome to the Fortune Teller!! \n" +
                 "Type your first name");
             string fName = Console.ReadLine();
+
             Console.WriteLine($"ok {fName}, Now i need your last name.");
             string lName = Console.ReadLine();
+
             Console.WriteLine($"{lName}, thats a very nice last name");
             string name = (fName +" "+ lName);
-            Console.WriteLine($"ok, {name}, how old are you?");
-            String age = Console.ReadLine();
-            Console.WriteLine($"So your age is {age}? \n" +
-                $"You're kinda old... but I digress \n" +
-                $"What Month were you born in?");
-            
-            String monthastext = Console.ReadLine();
-            int date = Convert.ToInt32(monthastext);
 
-            Console.WriteLine(monthastext);
+            Console.WriteLine($"ok {name},using numerals, how old are you?");
+            String tAge = Console.ReadLine();
+            int age = Convert.ToInt32(tAge);
+
+            Console.WriteLine($"So your age is {age}? \n" +
+                $"Wow, you're kinda old... but I digress, \n" +
+                $"What Month were you born in?");
+            String monthAsText = Console.ReadLine();
+            int nMonth = Convert.ToInt32(monthAsText);
+
+            
+
+
+            Console.WriteLine(monthAsText);
 
             Console.ReadLine();
             
