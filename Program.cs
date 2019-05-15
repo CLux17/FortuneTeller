41a3ad341a5267403f21076ed03c3fa720692d39
CLux <cplux7@gmail.com>
2019-05-15 17:21:54 -0400

    end of class progress

--- Program.cs
+++ Program.cs
@@ -22,16 +22,60 @@ namespace FortuneTeller
 
             Console.WriteLine($"So your age is {age}? \n" +
                 $"Wow, you're kinda old... but I digress, \n" +
-                $"What Month were you born in?");
+                $"Using numerals, what month were you born in?\n" +
+                $"(Hint, January = 1, Febreuary = 2, etc. etc.");
             String monthAsText = Console.ReadLine();
             int nMonth = Convert.ToInt32(monthAsText);
 
-            
+            Console.WriteLine("$Ok, {name}, Now i need your favorite ROYGBIV color?\n" +
+                "Don't know what ROYGBIV is? type, \"help\"");
+            string tColor = Console.ReadLine();
+
+            while (tColor == "help")
+            {
+                Console.WriteLine("ROYGBIV is the primary colors, Red Orange, Yellow, Green, Blue, Indigo, and Violet.");
+                tColor = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Final question {name}, How many siblings do you have?");
+            int siblings = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fantastic, I have all the info I need to read your fortune,")
 
 
-            Console.WriteLine(monthAsText);
+
+
+            string color = "Unknown";
+            switch (tColor)
+            {
+                case "red":
+                    color = "Maserati";
+                    break;
+                case "orange":
+                    color = "stallion";
+                    break;
+                case "yellow":
+                    color = "chariot";
+                    break;
+                case "green":
+                    color = "taxi";
+                    break;
+                case "blue":
+                    color = "rickshaw";
+                    break;
+                case "indigo":
+                    color = "motor scooter";
+                    break;
+                case "violet":
+                    color = "flying saucer";
+                    break;
+            }
+
+
 
             Console.ReadLine();
+            
+            
             
         }
     }
