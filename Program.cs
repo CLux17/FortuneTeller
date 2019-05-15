5fa4978a192a38339e04d32a86993ce4db824b33
CLux <cplux7@gmail.com>
2019-05-14 20:48:22 -0400

    added name lines

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace FortuneTeller
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Greetings!! and welcome to the Fortune Teller!! \n" +
+                "Type your name");
+            string name = Console.ReadLine();
+            if (name == "your name")
+            {
+                Console.WriteLine("Ha Ha Ha... very funny...");
+                Console.WriteLine("now your name is fool");
+                name = "fool";
+            }
+            else
+            {
+                Console.WriteLine($"Hello, {name}");
+            }
+            
+        
+            Console.WriteLine($"Now {name}, I need you to answer a few more questions...");
+            Console.ReadLine();
         }
     }
 }
