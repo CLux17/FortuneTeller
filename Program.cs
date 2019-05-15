9acf6912a27509d0bf0084923d88e8e865da02e7
CLux17 <cplus7@gmail.com>
2019-05-15 19:32:29 -0400

    finished questions, need to finish results

--- Program.cs
+++ Program.cs
@@ -6,73 +6,141 @@ namespace FortuneTeller
     {
         static void Main(string[] args)
         {
+            //Greeting
             Console.WriteLine("Greetings!! and welcome to the Fortune Teller!! \n" +
+                "\n" +
+            //Ask For First Name
                 "Type your first name");
             string fName = Console.ReadLine();
 
-            Console.WriteLine($"ok {fName}, Now i need your last name.");
+            //ASk for last name
+            Console.WriteLine($"Ok {fName}, Now I need your last name.");
             string lName = Console.ReadLine();
 
+            //Comment to confirm name and combine first and last
             Console.WriteLine($"{lName}, thats a very nice last name");
             string name = (fName +" "+ lName);
 
-            Console.WriteLine($"ok {name},using numerals, how old are you?");
-            String tAge = Console.ReadLine();
-            int age = Convert.ToInt32(tAge);
+            //Aquire age
+            Console.WriteLine($"Alright {name}, using numerals, how old are you?");
+            int nAge = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"So your age is {age}? \n" +
+            //Comment on age,  and aquire month
+            Console.WriteLine($"So your age is {nAge}? \n" +
                 $"Wow, you're kinda old... but I digress, \n" +
+                $"\n" +
                 $"Using numerals, what month were you born in?\n" +
                 $"(Hint, January = 1, Febreuary = 2, etc. etc.");
-            String monthAsText = Console.ReadLine();
-            int nMonth = Convert.ToInt32(monthAsText);
+            int nMonth = Convert.ToInt32(Console.ReadLine());
 
+            //aquire favorite color
             Console.WriteLine("$Ok, {name}, Now i need your favorite ROYGBIV color?\n" +
-                "Don't know what ROYGBIV is? type, \"help\"");
+                "Don't know what ROYGBIV is? type,\"help\"");
             string tColor = Console.ReadLine();
 
-            while (tColor == "help")
+            //provide assistance for user if not know ROYGBIV
+            while (tColor.ToLower() == "help")
             {
                 Console.WriteLine("ROYGBIV is the primary colors, Red Orange, Yellow, Green, Blue, Indigo, and Violet.");
                 tColor = Console.ReadLine();
             }
 
+            //Aquire # of siblings
             Console.WriteLine($"Final question {name}, How many siblings do you have?");
-            int siblings = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Fantastic, I have all the info I need to read your fortune,")
+            int nSiblings = Convert.ToInt32(Console.ReadLine());
 
 
+            Console.WriteLine("Fantastic, I have all the info I need to read your fortune!\n");
+            Console.ReadLine();
+            Console.WriteLine("\n" +
+                "\n" +
+                "Push Y and enter if you dare to tempt the fates and know their plans...\n" +
+                "\n" +
+                "Push N and enter if you dare not tempt the fates...");
+            string fate = Console.ReadLine();
+
+           
 
 
-            string color = "Unknown";
-            switch (tColor)
+            if (fate.ToLower() == "y")
             {
-                case "red":
-                    color = "Maserati";
-                    break;
-                case "orange":
-                    color = "stallion";
-                    break;
-                case "yellow":
-                    color = "chariot";
-                    break;
-                case "green":
-                    color = "taxi";
-                    break;
-                case "blue":
-                    color = "rickshaw";
-                    break;
-                case "indigo":
-                    color = "motor scooter";
-                    break;
-                case "violet":
-                    color = "flying saucer";
-                    break;
+
+                //Solution for Age
+                int age = 1;
+                if (nAge%2 == 0)
+                    {
+                        int age = 24;
+                    }
+                else
+                    {
+                        int age = 17;
+                    }
+
+                //Solution for Month
+                int income = 0;
+                if (nMonth <= 4)
+                    {
+                        int income = 3;
+                    }
+                else if (nMonth >= 5 || nMonth <= 8)
+                    {
+                        int income = 257843;
+                    }
+                else if (nMonth >= 9)
+                    {
+                        float income = 45283;
+                    }    
+                
+                //Solution for color choice
+                string color = "Unknown";
+                switch (tColor.ToLower())
+                {
+                    case "red":
+                        color = "Maserati";
+                        break;
+                    case "orange":
+                        color = "stallion";
+                        break;
+                    case "yellow":
+                        color = "chariot";
+                        break;
+                    case "green":
+                        color = "taxi";
+                        break;
+                    case "blue":
+                        color = "rickshaw";
+                        break;
+                    case "indigo":
+                        color = "motor scooter";
+                        break;
+                    case "violet":
+                        color = "flying saucer";
+                        break;
+                }
+
+                //Siblings
+                int siblings = 1;
+
+
+                //Answer
+                Console.WriteLine($"{name}, You will retire in {age} years, \n" +
+                    $"your bank account will at that time have {income} Dollars in it, \n" +
+                    $"You will have a vacation home in {siblings}, \n" +
+                    $"And your main mode of transportation will be by {color}");
+
             }
 
+            else if (fate.ToLower() == "n")
+
+                {
+                    Console.WriteLine("You have chosen well. Best to not tempt the fates");
+                }
 
 
+            Console.WriteLine("");
+            Console.WriteLine($"Thank you {name} for participating in this reading.\n" +
+                $"\n" +
+                $"Goodbye.");
             Console.ReadLine();
             
             
